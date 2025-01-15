4d927d2f8193f5480ffea68fc3498d24cf41be02
Luiz Henrique Caldas <luizhpcaldas@gmail.com>
2025-01-15 09:21:53 -0300

    create: Home views

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -8,6 +8,8 @@ class Home extends CI_Controller{
     }
 
 public function index() {
+    $this->load->view('layout/header');
     $this->load->view('home/index');
+    $this->load->view('layout/footer');
 }
 }
