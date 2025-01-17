2d9406c8b8898c8e687e867ac5c0b112e145f07c
Luiz Henrique Caldas <luizhpcaldas@gmail.com>
2025-01-17 09:54:48 -0300

    feat: create user tables and implement user listing in the system

---
+++ application/controllers/Usuarios.php
@@ -0,0 +1,18 @@
+<?php
+
+defined('BASEPATH') OR exit('Acao nao permitida');
+
+class Usuarios extends CI_Controller{
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
+        $data = array(
+            'usuarios' => $this->ion_auth->users()->result(),
+        );
+        $this->load->view('layout/header', $data);
+        $this->load->view('usuarios/index');
+        $this->load->view('layout/footer');
+    }
+}
